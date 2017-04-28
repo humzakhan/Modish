b9803a29bbe453e754ff7b4c0307f2a06309a851
Humza Khan <Humza Khan>
2017-04-29 01:08:57 +0500

    Add inputs.html and inject sidebr

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
 var expect = require('gulp-expect-file');
+var inject = require('gulp-inject');
 
 gulp.task('watch:sass', function() {
     gulp.watch(['src/scss/*.scss', 'src/scss/core/**/*', 'src/scss/components/**/*', 'src/scss/**/*'], ['build:sass'])
@@ -27,6 +28,19 @@ gulp.task('build:js', function() {
             suffix: ".min"
         }))
         .pipe(gulp.dest('dist/assets/js'));
+});
+
+gulp.task('inject:sidebar', function () {
+  return gulp.src('dist/*.html')
+    .pipe(inject(gulp.src(['src/html/sidebar.html']), {
+      starttag: '<!-- inject:SIDEBAR -->',
+      endtag: '<!-- inject:/SIDEBAR -->',
+      relative: true,
+      transform: function (filePath, file) {
+        return file.contents.toString('utf8')
+      }
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('build', ['build:sass', 'build:js']);
