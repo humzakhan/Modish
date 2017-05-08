ecdfc45f7bd5d19a0139617a5d888bc624ee55f7
Humza Khan <Humza Khan>
2017-05-08 16:44:10 +0500

    Copy bower components to dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,20 @@ var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
 var expect = require('gulp-expect-file');
 var inject = require('gulp-inject');
+var mainBowerFiles = require('main-bower-files');
 
-gulp.task('watch:sass', function() {
+gulp.task('watch:sass', function () {
     gulp.watch(['src/scss/*.scss', 'src/scss/core/**/*', 'src/scss/components/**/*', 'src/scss/**/*'], ['build:sass'])
 });
 
-gulp.task('build:sass', function() {
+gulp.task('build:sass', function () {
     return gulp.src('src/scss/*.scss')
         .pipe(plumber())
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/assets/css'));
 });
 
-gulp.task('build:js', function() {
+gulp.task('build:js', function () {
     return gulp.src('src/js/*.js')
         .pipe(uglify().on('error', util.log))
         .pipe(rename({
@@ -31,17 +32,24 @@ gulp.task('build:js', function() {
 });
 
 gulp.task('inject:sidebar', function () {
-  return gulp.src('dist/*.html')
-    .pipe(inject(gulp.src(['src/html/sidebar.html']), {
-      starttag: '<!-- inject:SIDEBAR -->',
-      endtag: '<!-- inject:/SIDEBAR -->',
-      relative: true,
-      transform: function (filePath, file) {
-        return file.contents.toString('utf8')
-      }
-    }))
-    .pipe(gulp.dest('dist/'));
+    return gulp.src('dist/*.html')
+        .pipe(inject(gulp.src(['src/html/sidebar.html']), {
+            starttag: '<!-- inject:SIDEBAR -->',
+            endtag: '<!-- inject:/SIDEBAR -->',
+            relative: true,
+            transform: function (filePath, file) {
+                return file.contents.toString('utf8')
+            }
+        }))
+        .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('deploy:copy-assets', function () {
+    return gulp.src(mainBowerFiles())
+        .pipe(gulp.dest('dist/bower_components'));
+});
+
+gulp.task('deploy', ['deploy:copy-assets']);
 
 gulp.task('build', ['build:sass', 'build:js']);
 
